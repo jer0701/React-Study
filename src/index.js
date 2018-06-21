97b4acaf17507644a63f4f89f0486103fbcec9f3
jer0701 <1018995917@qq.com>
2018-06-21 18:10:59 +0800

    flux and redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import ClickCounter from './ClickCounter';
-import ControlPanel from './ControlPanel';
+import ControlPanel from './redux/ControlPanel';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<ControlPanel />, document.getElementById('root'));
