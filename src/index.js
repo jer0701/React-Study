abf8d6c6338c80188d6d0f327eee9d402e7575f5
jer0701 <1018995917@qq.com>
2018-07-04 18:18:33 +0800

    todo

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {Provider} from 'react-redux';
-import store from './react-redux/Store';
-import ClickCounter from './ClickCounter';
-import ControlPanel from './react-redux/ControlPanel';
+import TodoApp from './todo/TodoApp';
+import store from './todo/Store.js';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <Provider store={store}>
-        <ControlPanel />
+        <TodoApp />
     </Provider>
     , document.getElementById('root'));
 registerServiceWorker();
